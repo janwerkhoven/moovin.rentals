ab43ef8b64cf935c247b4f00206b390e45b69f13
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-08-08 13:09:38 +1000

    Add remote deploy and install scripts

--- remote/install.sh
+++ remote/install.sh
@@ -18,8 +18,8 @@ echo "Path: $PWD"
 echo "Branch: $BRANCH"
 echo "Revision: $REVISION"
 echo "----------"
-echo "cd /var/www/moovin.rentals"
-cd /var/www/moovin.rentals
+echo "cd /var/www/reddust.org.au"
+cd /var/www/reddust.org.au
 echo "----------"
 echo "git checkout $BRANCH -f"
 git checkout $BRANCH -f
